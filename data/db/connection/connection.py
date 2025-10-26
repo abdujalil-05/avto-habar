a8965cf71bc105f1c005514d565eb14b72572abb
abdujalil-05 <dasturchi.bek.05@gmail.com>
2025-10-26 13:57:07 +0500

    Update connection.py

--- data/db/connection/connection.py
+++ data/db/connection/connection.py
@@ -1,5 +1,68 @@
-import aiomysql
+# import aiomysql
+# import redis.asyncio as redis
+
+# _redis = None
+# _pool = None
+
+
+# async def init_redis():
+#     global _redis
+#     if _redis is None:
+#         _redis = await redis.Redis.from_url(
+            
+#             "host": os.getenv("REDIS_HOST"),
+#             "user": os.getenv("REDIS_USER"),
+#             "password": os.getenv("REDIS_PASSWORD"),
+#             "port": int(os.getenv("REDIS_PORT", 3306))
+#             encoding="utf-8",
+#             decode_responses=True,
+#             max_connections=32
+#         )
+#         pong = await _redis.ping()
+#         if pong:
+#             print("✅ Redis ishlamoqda!")
+#         else:
+#             print("❌ Redis javob bermadi!")
+
+# # Pooldan get qilish misoli
+# async def get_redis():
+#     global _redis
+#     if _redis is None:
+#         await init_redis()
+#     return _redis
+
+
+# async def init_db_pool():
+#     global _pool
+#     if _pool is None:
+#         _pool = await aiomysql.create_pool(
+
+#             "host": os.getenv("MYSQL_HOST"),
+#             "user": os.getenv("MYSQL_USER"),
+#             "password": os.getenv("MYSQL_PASSWORD"),
+#             "database": os.getenv("MYSQL_DATABASE"),
+#             "port": int(os.getenv("MYSQL_PORT", 3306))
+
+
+#             # host="localhost",
+#             # user="root",
+#             # password="Aa20050309@",
+#             # db="avto_message",
+#             # port=3306,
+#             # minsize=1,
+#             # maxsize=32,
+#             # autocommit=True
+#         )
+
+
+# async def get_pool():
+#     if not _pool is None:
+#         return _pool
+    
+
+import os
 import redis.asyncio as redis
+import aiomysql
 
 _redis = None
 _pool = None
@@ -8,12 +71,10 @@ _pool = None
 async def init_redis():
     global _redis
     if _redis is None:
-        _redis = await redis.Redis.from_url(
-            
-            "host": os.getenv("REDIS_HOST"),
-            "user": os.getenv("REDIS_USER"),
-            "password": os.getenv("REDIS_PASSWORD"),
-            "port": int(os.getenv("REDIS_PORT", 3306))
+        redis_url = f"redis://{os.getenv('REDIS_USER')}:{os.getenv('REDIS_PASSWORD')}@{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT', 6379)}/0"
+
+        _redis = redis.from_url(
+            redis_url,
             encoding="utf-8",
             decode_responses=True,
             max_connections=32
@@ -24,7 +85,7 @@ async def init_redis():
         else:
             print("❌ Redis javob bermadi!")
 
-# Pooldan get qilish misoli
+
 async def get_redis():
     global _redis
     if _redis is None:
@@ -36,27 +97,19 @@ async def init_db_pool():
     global _pool
     if _pool is None:
         _pool = await aiomysql.create_pool(
-
-            "host": os.getenv("MYSQL_HOST"),
-            "user": os.getenv("MYSQL_USER"),
-            "password": os.getenv("MYSQL_PASSWORD"),
-            "database": os.getenv("MYSQL_DATABASE"),
-            "port": int(os.getenv("MYSQL_PORT", 3306))
-
-
-            # host="localhost",
-            # user="root",
-            # password="Aa20050309@",
-            # db="avto_message",
-            # port=3306,
-            # minsize=1,
-            # maxsize=32,
-            # autocommit=True
+            host=os.getenv("MYSQL_HOST"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            db=os.getenv("MYSQL_DATABASE"),
+            port=int(os.getenv("MYSQL_PORT", 3306)),
+            minsize=1,
+            maxsize=32,
+            autocommit=True
         )
 
 
 async def get_pool():
-    if not _pool is None:
-        return _pool
-    
-    
+    global _pool
+    if _pool is None:
+        await init_db_pool()
+    return _pool
